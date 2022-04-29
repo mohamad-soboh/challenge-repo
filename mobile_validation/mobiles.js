bf48693dbbbe9831203ac9b8db64781f9a25d614
mohamad-soboh <63787799+mohamad-soboh@users.noreply.github.com>
2022-04-29 14:34:01 +0300

    connecting mongo with express

--- mobile_validation/mobiles.js
+++ mobile_validation/mobiles.js
@@ -1,6 +1,12 @@
+//load express
 const express =require('express');
 const app = express();
 
+//load mangoose
+const mongoose =require("mongoose");
+mongoose.connect("mongodb+srv://admin:admin@cluster0.qhek7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",()=>{
+    console.log("Database Connected")
+});
 app.get('/',(req,res)=>{
     res.send("api running !")});
 
