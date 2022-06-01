c8d2bed7c39e8dc509a996a8aec20893b6b075d9
mohamad-soboh <63787799+mohamad-soboh@users.noreply.github.com>
2022-06-01 16:07:19 +0300

    merging brachnes

---
+++ backEnd/api/customerRoute.js
@@ -0,0 +1,46 @@
+//bring in express
+const express = require("express");
+//importing customer controller
+const router = express.Router();
+const res = require("express/lib/response");
+
+const customerController =require('../api/customerController')
+//for proxy
+const cors = require("cors");
+router.use(
+  cors({
+    origin: "*",
+  })
+);
+//use express router
+
+
+
+//@route  GET api/users
+//@desc   Get All Customers
+//@acess  Public/
+router.get("/AllCustomers",customerController.customer_index);
+
+
+//@route  POST api/users
+//@desc   Add a Customer
+//@acess  Public
+router.post("/Addcustomer",customerController.customer_create);
+
+
+//@route  PATCH api/users
+//@desc   Update a Customer
+//@acess  Public/
+router.patch("/update/:id",customerController.customer_update);
+
+
+//@route  DELETE api/users
+//@desc   Delete a Customer
+//@acess  Public/
+router.delete("/DeleteCustomer/:id",customerController.customer_delete);
+
+
+
+
+//export all the routers
+module.exports = router;
