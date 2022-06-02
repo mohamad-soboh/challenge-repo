a1246b4b8fde8b4be6764d8c383360295e10f541
mohamad-soboh <63787799+mohamad-soboh@users.noreply.github.com>
2022-06-02 15:06:38 +0300

    mergred

--- client/src/App.js
+++ client/src/App.js
@@ -1,92 +1,80 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import createCustomer from "/Users/mohamadsoboh/ReactNativeProjects/challenge-repo/client/src/createCustomerView.js";
+import { Formik, useFormik, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 export default function App() {
   const [customers, setCustomers] = useState([]);
-  //storing the  form values as an obj in single state hook
-  const [addFormData, setAddFormData] = useState({
-    fullName: "",
-    adress: "",
-    mobileNumber: "",
-  });
-  const [phoneValidate, setPhoneValidate] = useState([1]);
-  //state to handle form erros
-  const [updateFormData, setUpdateFormData] = useState({
-    fullName: "",
-    adress: "",
-    mobileNumber: "",
-    id:""
-  });
   const [Deleted, setDeleted] = useState(1);
- 
-  //validating  the form
-  // const validate = (mobile)=>
-  // axios.get(`https://phonevalidation.abstractapi.com/v1/?api_key=30a0870189ae4438ac41958b995fc39b&phone=${mobile}`)
-  // .then((response) => setPhoneValidate([...response.data]));
-  
-  const disp =()=>
-   {
-     console.log(phoneValidate);
-   }
-  //using a handler to update the form values
-  const handleAddFormChange = (event) => {
-    event.preventDefault();
-    console.log(event.target.value);
-    const fieldName = event.target.getAttribute("name");
-    const fieldValue = event.target.value;
-
-    //making a cope from the exisit form data so we can mutate it
-    const newFormData = { ...addFormData };
-
-    //update the obj with the new value the user has tiped
-    newFormData[fieldName] = fieldValue;
-    setAddFormData(newFormData);
-    console.log(addFormData);
-  };
-  //loading customer data into form and upading the values
-  const handleEditFormChange = (event) => {
-    event.preventDefault();
-    const fieldName = event.target.getAttribute("name");
-    const fieldValue = event.target.value;
-    //making a cope from the exisit form data so we can mutate it
-    const newFormData = { ...updateFormData };
-
-    //update the obj with the new value the user has tiped
-    newFormData[fieldName] = fieldValue;
-    setUpdateFormData(newFormData);
-    console.log(updateFormData);
-  };
+  const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+  const forimk = useFormik({
+    initialValues: {
+      fullName: "",
+      adress: "",
+      mobileNumber: "",
+    },
+    validationSchema: Yup.object({
+      fullName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      adress: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      mobileNumber: Yup.string()
+        .max(15, "Must be 20 characters or less")
+        .matches(phoneRegExp, "Phone number is not valid")
+        .required("Required"),
+    }),
+    onSubmit: () => {
+      onCreateCustomer();
+    },
+  });
+  const updatedFormik = useFormik({
+    initialValues: {
+      fullName: "",
+      adress: "",
+      mobileNumber: "",
+      id: "",
+    },
+    validationSchema: Yup.object({
+      fullName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Required"),
+      adress: Yup.string()
+        .max(20, "Must be 20 characters or less")
+        .required("Required"),
+      mobileNumber: Yup.string()
+        .max(15, "Must be 20 characters or less")
+        .matches(phoneRegExp, "Phone number is not valid")
+        .required("Required"),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+      onUpdateCustomer();
+    },
+  });
+  //should be the formik submit
   const handleUpdateCustomerClick = (customer) => {
-    
-    console.log(customer._id);
-    const mobile =customer.customer_mobile_number;
-    const formValues = {
-      fullName: customer.customer_name,
-      adress: customer.customer_adress,
-      mobileNumber:customer.customer_mobile_number,
-      id:customer._id
-    };
-    setUpdateFormData(formValues);
-    console.log(updateFormData);
+    updatedFormik.setFieldValue("fullName", customer.customer_name);
+    updatedFormik.setFieldValue("adress", customer.customer_adress);
+    updatedFormik.setFieldValue(
+      "mobileNumber",
+      customer.customer_mobile_number
+    );
+    updatedFormik.setFieldValue("id", customer._id);
   };
   useEffect(() => {
     GetAllCustomers();
-}, [Deleted]);
-// useEffect(() => {
-//   const interval = setInterval(() => {
-// GetAllCustomers();
-//   }, 500);
-//   return () => clearInterval(interval);
-// }, []);
+  }, [Deleted]);
 
   // Requesting GET All Customer Api
   const GetAllCustomers = () => {
     axios
       .get("http://localhost:4545/api/customers/AllCustomers")
       .then((response) => {
-        setCustomers([...response.data])
-        console.log(response)
+        setCustomers([...response.data]);
+        console.log(response);
       });
     //  if (!response.ok) {
     //     throw new Error("Something went wrong!");
@@ -97,152 +85,186 @@ export default function App() {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      fullName: addFormData.fullName,
-      adress: addFormData.adress,
-      mobileNumber: addFormData.mobileNumber,
+      customer_name: forimk.values.fullName,
+      customer_adress: forimk.values.adress,
+      customer_mobile_number: forimk.values.mobileNumber,
     }),
   };
   const requestUpdateOptions = {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      customer_name: updateFormData.fullName,
-      customer_adress: updateFormData.adress,
-      customer_mobile_number: updateFormData.mobileNumber,
+      customer_name: updatedFormik.values.fullName,
+      customer_adress: updatedFormik.values.adress,
+      customer_mobile_number: updatedFormik.values.mobileNumber,
     }),
   };
   //creating a customer using Add Customer Api
 
   const onCreateCustomer = () => {
-    fetch("http://localhost:4545/api/customers/Addcustomer", requestOptions).then(
-      (response) =>  console.log(response))  };
+    console.log(requestOptions);
+    fetch(
+      "http://localhost:4545/api/customers/Addcustomer",
+      requestOptions
+    ).then((response) => {
+      console.log(response);
+      setDeleted(0);
+    });
+    setDeleted(1);
+  };
   //Deleting a customer
   const onDeleteCustomer = (customerId) => {
     console.log(customerId);
-    axios.delete(`http://localhost:4545/api/customers/DeleteCustomer/${customerId}`).then((response) =>
-    {
-      console.log(response)
-      setDeleted(0);
-    }
-  );
+    axios
+      .delete(
+        `http://localhost:4545/api/customers/DeleteCustomer/${customerId}`
+      )
+      .then((response) => {
+        console.log(response);
+        setDeleted(0);
+      });
+    setDeleted(1);
   };
 
   //update a customer
-  const onUpdateCustomer = (updateFormData) => {
-    fetch(`http://localhost:4545/api/customers/update/${updateFormData.id}`, requestUpdateOptions).then((response) =>
-      console.log(response)
-    );
+  const onUpdateCustomer = () => {
+    fetch(
+      `http://localhost:4545/api/customers/update/${updatedFormik.values.id}`,
+      requestUpdateOptions
+    ).then((response) => {
+      console.log(response);
+      setDeleted(0);
+    });
+    setDeleted(1);
   };
   return (
-
-
-
     <div style={{ marginTop: "15px" }}>
- {customers.length == 0 ?  <h2>You dont have any customer records</h2>: 
-      <table>
-        <thead>
-          <tr>
-            <th style={{ textAlign: "center" }}>No.</th>
-            <th style={{ textAlign: "center" }}>name</th>
-            <th style={{ textAlign: "center" }}>Adress</th>
-            <th style={{ textAlign: "center" }}>Mobile</th>
-            <th style={{ textAlign: "center" }}>actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {customers.map((customer, index) => {
-            return (
-              <tr key={customer._id}>
-                <th style={{ textAlign: "center" }} scrope="row">
-                  {index + 1}
-                </th>
-                <td style={{ textAlign: "center" }}>
-                  {customer.customer_name}
-                </td>
-                <td style={{ textAlign: "center" }}>
-                  {customer.customer_adress}
-                </td>
-                <td style={{ textAlign: "center" }}>
-                  {customer.customer_mobile_number}
-                </td>
-                <td>
-                  <button onClick={() => onDeleteCustomer(customer._id)}>
-                    Delete
-                  </button>
-                  <button onClick={() => handleUpdateCustomerClick(customer)}>
-                    Update
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-}
+      {customers.length == 0 ? (
+        <h2>You dont have any customer records</h2>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th style={{ textAlign: "center" }}>No.</th>
+              <th style={{ textAlign: "center" }}>name</th>
+              <th style={{ textAlign: "center" }}>Adress</th>
+              <th style={{ textAlign: "center" }}>Mobile</th>
+              <th style={{ textAlign: "center" }}>actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {customers.map((customer, index) => {
+              return (
+                <tr key={customer._id}>
+                  <th style={{ textAlign: "center" }} scrope="row">
+                    {index + 1}
+                  </th>
+                  <td style={{ textAlign: "center" }}>
+                    {customer.customer_name}
+                  </td>
+                  <td style={{ textAlign: "center" }}>
+                    {customer.customer_adress}
+                  </td>
+                  <td style={{ textAlign: "center" }}>
+                    {customer.customer_mobile_number}
+                  </td>
+                  <td>
+                    <button onClick={() => onDeleteCustomer(customer._id)}>
+                      Delete
+                    </button>
+                    <button onClick={() => handleUpdateCustomerClick(customer)}>
+                      Update
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
       <h3>Add a Customer</h3>
       <form>
         <input
           type="text"
           name="fullName"
-          value={addFormData.fullName.replace(/[^a-z\s]/gi, "")}
           required="reuired"
           placeholder="Enter name"
-          onChange={handleAddFormChange}
-        ></input>
+          value={forimk.values.fullName}
+          onChange={forimk.handleChange}
+          onBlur={forimk.handleBlur}
+        />
+        {forimk.touched.fullName && forimk.errors.fullName ? (
+          <p>{forimk.errors.fullName} </p>
+        ) : null}
         <input
           type="text"
           name="adress"
           required="reuired"
           placeholder="Enter an adress"
-          onChange={handleAddFormChange}
-        ></input>
+          value={forimk.values.adress}
+          onChange={forimk.handleChange}
+          onBlur={forimk.handleBlur}
+        />
+        {forimk.touched.adress && forimk.errors.adress ? (
+          <p>{forimk.errors.adress} </p>
+        ) : null}
         <input
           type="text"
           name="mobileNumber"
           required="reuired"
           placeholder="Enter mobile number"
-          value={addFormData.mobileNumber.replace(/[^0-9]/, "")}
-          onChange={handleAddFormChange}
-        ></input>
-        <button type="submit" onClick={onCreateCustomer}>
-        Submit
-        </button>
-        {/* <button>validate</button> */}
+          value={forimk.values.mobileNumber}
+          onChange={forimk.handleChange}
+          onBlur={forimk.handleBlur}
+        />
+        {forimk.touched.mobileNumber && forimk.errors.mobileNumber ? (
+          <p>{forimk.errors.mobileNumber} </p>
+        ) : null}
+        <button type="submit">Submit</button>
       </form>
 
       <h3>update Customer</h3>
-      <form>
+      <form onSubmit={updatedFormik.handleSubmit}>
         <input
           type="text"
           name="fullName"
-          value={updateFormData.fullName.replace(/[^a-z\s]/gi, "")}
           required="reuired"
           placeholder="Enter name"
-          onChange={handleEditFormChange}
-          
-        ></input>
+          value={updatedFormik.values.fullName}
+          onChange={updatedFormik.handleChange}
+          onBlur={updatedFormik.handleBlur}
+        />
+        {updatedFormik.touched.fullName && updatedFormik.errors.fullName ? (
+          <p>{updatedFormik.errors.fullName} </p>
+        ) : null}
+
         <input
           type="text"
           name="adress"
           required="reuired"
           placeholder="Enter an adress"
-          value={updateFormData.adress}
-          onChange={handleEditFormChange}
-        ></input>
+          value={updatedFormik.values.adress}
+          onChange={updatedFormik.handleChange}
+          onBlur={updatedFormik.handleBlur}
+        />
+        {updatedFormik.touched.adress && updatedFormik.errors.adress ? (
+          <p>{updatedFormik.errors.adress} </p>
+        ) : null}
+
         <input
           type="text"
           name="mobileNumber"
-          required="reuired"
-          placeholder="Enter mobile number"
-          value={updateFormData.mobileNumber.toString().replace(/[^0-9]/, "")}
-          onChange={handleEditFormChange}
-        ></input>
-        <button type="submit" onClick={() => onUpdateCustomer(updateFormData)}>
-          Submit
-        </button>
-      </form>
+          value={updatedFormik.values.mobileNumber}
+          onChange={updatedFormik.handleChange}
+          onBlur={updatedFormik.handleBlur}
+        />
+        {updatedFormik.touched.mobileNumber &&
+        updatedFormik.errors.mobileNumber ? (
+          <p>{updatedFormik.errors.mobileNumber} </p>
+        ) : null}
 
-     
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
