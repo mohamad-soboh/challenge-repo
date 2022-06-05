4658a092ed1516fc19ab0649cb3a72407d0b6f2b
mohamad-soboh <63787799+mohamad-soboh@users.noreply.github.com>
2022-06-05 13:42:31 +0300

    Update server.ts

--- backEnd/server.ts
+++ backEnd/server.ts
@@ -1,28 +1,60 @@
+
+
+
+
+
+
+
 import express, { Application, Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser"
-import router from './api/customerAPI/customerRoute'
-const app = express();
-import connectDB from "../config/db"
+import connect from "../config/db";
+import customerRouter from './api/customerAPI/customerRoute'
+
+const app: Application = express();
+const port: number = 4545;
+const db: string = "mongodb+srv://admin:admin@cluster0.qhek7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// init Middlware
-// app.use(express.json({extended :false}));
-connectDB;
-app.get('/',(req,res)=> res.send('API Running'));
 
-const cors = require('cors');
-app.use(cors({
-    origin:  '*'
-}));
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("api running");
+});
 
-//define Routes 
-app.use('/api/customers',router);
+// //define Routes 
+app.use('/api/customers',customerRouter);
 
-
-const PORT = process.env.PORT || 8081;
-
-app.listen(4545, () => {
-  console.log("server started ! ");
+connect(db);
+app.listen(port, () => {
+  console.log(`Server running `);
 });
 
 
+// import express, { Application, Request, Response, NextFunction } from "express";
+// import bodyParser from "body-parser"
+// import router from './api/customerAPI/customerRoute'
+// const app = express();
+// import connectDB from "../config/db"
+
+
+// // init Middlware
+// // app.use(express.json({extended :false}));
+// connectDB;
+// app.get('/',(req,res)=> res.send('API Running'));
+
+// const cors = require('cors');
+// app.use(cors({
+//     origin:  '*'
+// }));
+
+// //define Routes 
+// app.use('/api/customers',router);
+
+
+// const PORT = process.env.PORT || 8081;
+
+// app.listen(4545, () => {
+//   console.log("server started ! ");
+// });
